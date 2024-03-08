88a8e22cfe444136105b9000958eae21ff61f72c
goutham817 <158554789+goutham817@users.noreply.github.com>
2024-03-08 12:55:31 +0530

    functions

--- 04-functions.sh
+++ 04-functions.sh
@@ -1,5 +1,15 @@
 example() {
   echo hello world
 }
-# call function
+
 example
+
+# function has its own special variables
+
+example1() {
+  echo value of 1 -$1
+  echo "value of * - $*"
+  echo "value of # - $#"
+}
+
+example1 10 20 30

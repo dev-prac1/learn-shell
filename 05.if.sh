23758c086c023973c23333a753b036afbd2fe260
goutham817 <158554789+goutham817@users.noreply.github.com>
2024-03-12 14:43:05 +0530

    password missing

---
+++ 05.if.sh
@@ -0,0 +1,37 @@
+## Example for multi line comment
+
+<<EOF
+
+simple if
+
+if [ expression ]; then
+  commands
+  fi
+
+  if else
+
+  if [ expression ]; then
+     commands
+  else
+     commands
+   fi
+
+   else if
+
+   if [ expression ]; then
+     commands
+    elif [ expression2 ]; then
+      commands
+   else
+      commands
+  fi
+
+EOF
+
+# Expression
+
+# 1. number expression
+
+# 2. string expression
+
+# 3. file expression
